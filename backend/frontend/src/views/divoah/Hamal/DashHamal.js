ae31acc36f984050dc53f7fadabacac9baa86625
anat lakus <lakusan68@gmail.com>
2022-11-22 14:15:16 +0200

    com

--- backend/frontend/src/views/divoah/Hamal/DashHamal.js
+++ backend/frontend/src/views/divoah/Hamal/DashHamal.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 import { Link, withRouter, Redirect } from "react-router-dom";
 import {
@@ -14,26 +14,55 @@ import {
   InputGroupText,
   InputGroup,
   Row,
-  Col
+  Col,
 } from "reactstrap";
-import axios from 'axios';
-import history from 'history.js'
+import axios from "axios";
+import history from "history.js";
 import { toast } from "react-toastify";
 
+const AdminSignInForm = () => {
+  const [reportDB, setReportDB] = useState([]);
+  const [isError, setIsError] = useState(false);
+ const eventTypeArray = {
+  "בחר": "",
+  "1": "תאונת כלי רכב",
+  "2": "התהפכות",
+  "3": "הנתקות גלגל",
+  "4": "שריפה",
+  "5": "אירוע נשק / תחמושת",
+  "6": 'תאונת עבודה אנשי טנ"א',
+  "7": "פריקת מטפים",
+  "8": "אפידמיה",
+  "9": "חילוץ",
+  "10": 'נזק לתשתיות אחזקה / הח"י',
+  "11": "אי קיום שגרת אחזקה",
+  "12": "אחר",
+  "רק'ם": 'רק"ם',
+}
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8000/report/`)
+      .then((response) => {
+        console.log(response.data);
+        setReportDB(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+        setIsError(true);
+      });
+  }, []);
 
-function AdminSignInForm() {
-
-return (
-  <div>
-    <Container className="mt--8 pb-5">
+  return (
+    <div>
+      <Container className="mt--8 pb-5">
         <Row>
           <Col lg="6">
-          <Card className="card-chart">
-             <CardHeader>
+            <Card className="card-chart">
+              <CardHeader>
                 <h3 className="card-category text-center">טבלאת אירועים</h3>
               </CardHeader>
-            <CardBody>
-            <table className="tablesorter" responsive>
+              <CardBody>
+                <table className="tablesorter" responsive>
                   <thead className="text-primary">
                     <tr>
                       <th className="text-center">יחידה</th>
@@ -42,40 +71,54 @@ return (
                     </tr>
                   </thead>
                   <tbody>
+                    {reportDB.map((report, index) => (
+                      <tr>
+                        <td>
+                          <p>{report.yhida}</p>
+                        </td>
+                        <td>{eventTypeArray[report.typevent]}</td>
+                        <td>{report.pirot}</td>
+                      </tr>
+                    ))}
                   </tbody>
                 </table>
-
-            </CardBody>
-          </Card>
+              </CardBody>
+            </Card>
           </Col>
           <Col lg="6">
             <Row>
-             <Col lg="12">
-              <Card className="card-chart">
-              <CardHeader>
-                <h3 className="card-category text-center"> אירועים לפי פיקוד</h3>
-              </CardHeader>
-               <CardBody>
-                <h1>2</h1>
-               </CardBody>
-              </Card>
-             </Col>
-             <Col lg="12">
-              <Card className="card-chart">
-              <CardHeader>
-                <h3 className="card-category text-center"> אירועים לפי סוג אירוע</h3>
-              </CardHeader>
-               <CardBody>
-                <h1>3</h1>
-               </CardBody>
-              </Card>
-             </Col>
+              <Col lg="12">
+                <Card className="card-chart">
+                  <CardHeader>
+                    <h3 className="card-category text-center">
+                      {" "}
+                      אירועים לפי פיקוד
+                    </h3>
+                  </CardHeader>
+                  <CardBody>
+                    <h1>2</h1>
+                  </CardBody>
+                </Card>
+              </Col>
+              <Col lg="12">
+                <Card className="card-chart">
+                  <CardHeader>
+                    <h3 className="card-category text-center">
+                      {" "}
+                      אירועים לפי סוג אירוע
+                    </h3>
+                  </CardHeader>
+                  <CardBody>
+                    <h1>3</h1>
+                  </CardBody>
+                </Card>
+              </Col>
             </Row>
           </Col>
         </Row>
-    </Container>
-  </div>
-)
-}
+      </Container>
+    </div>
+  );
+};
 
 export default AdminSignInForm;
