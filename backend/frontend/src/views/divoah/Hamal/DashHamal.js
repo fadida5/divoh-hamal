ca23bd44a05e72c04fa3bcfa5f09a09507eb35ec
anat lakus <lakusan68@gmail.com>
2023-01-17 14:01:26 +0200

    statistic

--- backend/frontend/src/views/divoah/Hamal/DashHamal.js
+++ backend/frontend/src/views/divoah/Hamal/DashHamal.js
@@ -19,10 +19,21 @@ import {
 import axios from "axios";
 import history from "history.js";
 import { toast } from "react-toastify";
+import { Line, Pie, Doughnut, PolarArea } from 'react-chartjs-2';
+import Select from 'components/general/Select/AnimatedSelect'
+
 
 const AdminSignInForm = () => {
   const [reportDB, setReportDB] = useState([]);
   const [isError, setIsError] = useState(false);
+
+  const [data, setData] = useState([]);
+
+  const [gdods, setGdods] = useState([]);
+  const [hativas, setHativas] = useState([]);
+  const [ogdas, setOgdas] = useState([]);
+  const [pikods, setPikods] = useState([]);
+
  const eventTypeArray = {
   "בחר": "",
   "1": "תאונת כלי רכב",
@@ -39,18 +50,380 @@ const AdminSignInForm = () => {
   "12": "אחר",
   "רק'ם": 'רק"ם',
 }
-  useEffect(() => {
-    axios
-      .get(`http://localhost:8000/report/`)
-      .then((response) => {
-        console.log(response.data);
-        setReportDB(response.data);
+
+const loadPikods = async () => {
+  await axios.get("http://localhost:8000/api/pikod",)
+      .then(response => {
+          setPikods(response.data);
       })
       .catch((error) => {
-        console.log(error);
-        setIsError(true);
-      });
+          console.log(error);
+      })
+}
+
+const loadOgdas = async (pikodids) => {
+  let temppikodids = pikodids;
+  if (temppikodids != undefined && !temppikodids.isArray) {
+      temppikodids = [pikodids]
+  }
+  let temppikodsogdas = [];
+  if (temppikodids != undefined && temppikodids.length > 0) {
+      for (let i = 0; i < temppikodids.length; i++) {
+          await axios.post("http://localhost:8000/api/ogda/ogdasbypikodid", { pikod: temppikodids[i] })
+              .then(response => {
+                  for (let j = 0; j < response.data.length; j++)
+                      temppikodsogdas.push(response.data[j])
+              })
+              .catch((error) => {
+                  console.log(error);
+              })
+      }
+  }
+  setOgdas(temppikodsogdas);
+}
+
+const loadHativas = async (ogdaids) => {
+  let tempogdaids = ogdaids;
+  if (tempogdaids != undefined && !tempogdaids.isArray) {
+      tempogdaids = [ogdaids]
+  }
+  let tempogdashativas = [];
+  if (tempogdaids != undefined && tempogdaids.length > 0) {
+      for (let i = 0; i < tempogdaids.length; i++) {
+          await axios.post("http://localhost:8000/api/hativa/hativasbyogdaid", { ogda: tempogdaids[i] })
+              .then(response => {
+                  for (let j = 0; j < response.data.length; j++)
+                      tempogdashativas.push(response.data[j])
+              })
+              .catch((error) => {
+                  console.log(error);
+              })
+      }
+  }
+  setHativas(tempogdashativas);
+}
+
+const loadGdods = async (hativaids) => {
+  let temphativaids = hativaids;
+  if (temphativaids != undefined && !temphativaids.isArray) {
+      temphativaids = [hativaids]
+  }
+  let temphativasgdods = [];
+  if (temphativaids != undefined && temphativaids.length > 0) {
+      for (let i = 0; i < temphativaids.length; i++) {
+          await axios.post("http://localhost:8000/api/gdod/gdodsbyhativaid", { hativa: temphativaids[i] })
+              .then(response => {
+                  for (let j = 0; j < response.data.length; j++)
+                      temphativasgdods.push(response.data[j])
+              })
+              .catch((error) => {
+                  console.log(error);
+              })
+      }
+  }
+  setGdods(temphativasgdods);
+} 
+
+const loadReports = () => {
+  axios
+  .get(`http://localhost:8000/report/`)
+  .then((response) => {
+    console.log(response.data);
+    setReportDB(response.data);
+  })
+  .catch((error) => {
+    console.log(error);
+    setIsError(true);
+  });
+};
+
+function handleChange2(selectedOption, name) {
+  if (!(selectedOption.value == "בחר"))
+    setData({ ...data, [name]: selectedOption.value });
+  else {
+    let tempdata = { ...data };
+    delete tempdata[name];
+    setData(tempdata);
+  }
+}
+
+const options = {
+  responsive: true,
+  plugins: {
+      legend: {
+        display: false
+      },
+  },
+}
+
+const  labels = ['תאונת כלי רכב', 'התהפכות', 'הנתקות גלגל', 'שריפה', 'אירוע נשק / תחמושת', 'תאונת עבודה אנשי טנ"א', 'פריקת מטפים', 'אפידמיה', 'חילוץ', 'נזק לתשתיות אחזקה / הח"י', 'אי קיום שגרת אחזקה', 'אחר' , 'רק"ם'];
+
+function sumtypereport(arr1,arr2,arr3){
+  let alldata= [];
+  for(let i=0;i<arr1.length;i++){
+    let sum=0;
+    for(let j=0;j<arr2.length;j++){
+      if(arr3[arr2[j].typevent] == arr1[i])
+         sum ++;
+    }
+    alldata[i]=sum;
+  }
+  return alldata;
+}
+
+const dataevent = {
+  labels: labels,
+  datasets: [
+      {
+          label: '# of Votes',
+          data: sumtypereport(labels,reportDB,eventTypeArray),
+          backgroundColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)',
+            'rgba(157, 241, 223, 1)',
+            'rgba(130, 0, 0, 1)',
+            'rgba(78, 108, 80, 1)',
+            'rgba(207, 77, 206, 1)',
+            'rgba(61, 23, 102, 1)',
+            'rgba(0, 255, 246, 1)',
+            'rgba(255, 173, 188, 1)',
+        ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)',
+            'rgba(157, 241, 223, 1)',
+            'rgba(130, 0, 0, 1)',
+            'rgba(78, 108, 80, 1)',
+            'rgba(207, 77, 206, 1)',
+            'rgba(61, 23, 102, 1)',
+            'rgba(0, 255, 246, 1)',
+            'rgba(255, 173, 188, 1)',
+          ],
+          borderWidth: 1,
+      },
+  ],
+};
+
+function sumpikods(arr1,arr2){
+  let sumallpikods= [];
+  for(let i=0;i<arr1.length;i++){
+    let sum=0;
+    for(let j=0;j<arr2.length;j++){
+      if(arr2[j].pikod == arr1[i]._id)
+         sum ++;
+    }
+    sumallpikods[i]=sum;
+  }
+  return sumallpikods;
+}
+
+
+const datapikod = {
+  labels: pikods.map((pikod,index)=> pikod.name),
+  datasets: [
+      {
+          label: '# of Votes',
+          data: sumpikods(pikods,reportDB),
+          backgroundColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)',
+            'rgba(157, 241, 223, 1)',
+            'rgba(130, 0, 0, 1)',
+            'rgba(78, 108, 80, 1)',
+            'rgba(207, 77, 206, 1)',
+            'rgba(61, 23, 102, 1)',
+            'rgba(0, 255, 246, 1)',
+            'rgba(255, 173, 188, 1)',
+        ],
+          borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+              'rgba(157, 241, 223, 1)',
+              'rgba(130, 0, 0, 1)',
+              'rgba(78, 108, 80, 1)',
+              'rgba(207, 77, 206, 1)',
+              'rgba(61, 23, 102, 1)',
+              'rgba(0, 255, 246, 1)',
+              'rgba(255, 173, 188, 1)',
+
+          ],
+          borderWidth: 1,
+      },
+  ],
+};
+
+function getcolor(){
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+  return "rgb(" + r + "," + g + "," + b + ")";
+}
+
+function randomcolor(arr1){
+  const colors= [];
+  for(let i=0;i<arr1.length;i++){
+    colors.push(getcolor());
+    console.log(colors);
+  }
+  return colors;
+}
+
+function sumogda(arr1,arr2){
+  let sumallogdas= [];
+  for(let i=0;i<arr1.length;i++){
+    let sum=0;
+    for(let j=0;j<arr2.length;j++){
+      if(arr2[j].ogda == arr1[i]._id)
+         sum ++;
+    }
+    sumallogdas[i]=sum;
+  }
+  return sumallogdas;
+}
+
+const arryogda=ogdas.filter((ogda)=>ogda.pikod == data.pikod);
+
+const dataogda = {
+  labels: arryogda.map((ogda)=>ogda.name),
+  datasets: [
+      {
+          label: '# of Votes',
+          data: sumogda(arryogda,reportDB),
+          backgroundColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)',
+            'rgba(157, 241, 223, 1)',
+            'rgba(130, 0, 0, 1)',
+            'rgba(78, 108, 80, 1)',
+            'rgba(207, 77, 206, 1)',
+            'rgba(61, 23, 102, 1)',
+            'rgba(0, 255, 246, 1)',
+            'rgba(255, 173, 188, 1)',
+        ],
+          borderColor: [
+            'rgba(200, 200, 200, 0.75)',
+          ],
+          borderWidth: 1,
+      },
+  ],
+};
+
+function sumhativa(arr1,arr2){
+  let sumallhativas= [];
+  for(let i=0;i<arr1.length;i++){
+    let sum=0;
+    for(let j=0;j<arr2.length;j++){
+      if(arr2[j].hativa == arr1[i]._id)
+         sum ++;
+    }
+    sumallhativas[i]=sum;
+  }
+  return sumallhativas;
+}
+
+const arryhativa=hativas.filter((hativa)=>hativa.ogda == data.ogda);
+
+const datahativa = {
+  labels: arryhativa.map((hativa)=>hativa.name),
+  datasets: [
+      {
+          label: '# of Votes',
+          data: sumhativa(arryhativa,reportDB),
+          backgroundColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)',
+            'rgba(157, 241, 223, 1)',
+            'rgba(130, 0, 0, 1)',
+            'rgba(78, 108, 80, 1)',
+            'rgba(207, 77, 206, 1)',
+            'rgba(61, 23, 102, 1)',
+            'rgba(0, 255, 246, 1)',
+            'rgba(255, 173, 188, 1)',
+        ],
+          borderColor: [
+            'rgba(200, 200, 200, 0.75)',
+          ],
+          borderWidth: 1,
+      },
+  ],
+};
+
+function sumgdod(arr1,arr2){
+  let sumallgdods= [];
+  for(let i=0;i<arr1.length;i++){
+    let sum=0;
+    for(let j=0;j<arr2.length;j++){
+      if(arr2[j].gdod == arr1[i]._id)
+         sum ++;
+    }
+    sumallgdods[i]=sum;
+  }
+  return sumallgdods;
+}
+
+const arrygdod=gdods.filter((gdod)=>gdod.hativa == data.hativa);
+
+const datagdod = {
+  labels: arrygdod.map((gdod)=>gdod.name),
+  datasets: [
+      {
+          label: '# of Votes',
+          data: sumgdod(arrygdod,reportDB),
+          backgroundColor: randomcolor(arrygdod),
+          borderColor: [
+            'rgba(200, 200, 200, 0.75)',
+          ],
+          borderWidth: 1,
+      },
+  ],
+};
+
+
+
+
+  useEffect(() => {
+    loadReports();
+    loadPikods();
   }, []);
+
+  useEffect(() => {
+    setOgdas([]);
+    loadOgdas(data.pikod);
+  }, [data.pikod]);
+
+  useEffect(() => {
+    setHativas([]);
+    loadHativas(data.ogda);
+  }, [data.ogda]);
+
+  useEffect(() => {
+    setGdods([]);
+    loadGdods(data.hativa);
+  }, [data.hativa]);
 
   return (
     <div>
@@ -59,7 +432,7 @@ const AdminSignInForm = () => {
           <Col lg="6">
             <Card className="card-chart">
               <CardHeader>
-                <h3 className="card-category text-center">טבלאת אירועים</h3>
+                <h3 className="card-category text-center">טבלת אירועים</h3>
               </CardHeader>
               <CardBody>
                 <table className="tablesorter" responsive>
@@ -74,7 +447,7 @@ const AdminSignInForm = () => {
                     {reportDB.map((report, index) => (
                       <tr>
                         <td>
-                          <p>{report.pirot}</p>
+                          <p>{report.pikod}</p>
                         </td>
                         <td>{eventTypeArray[report.typevent]}</td>
                         <td>{report.pirot}</td>
@@ -85,37 +458,126 @@ const AdminSignInForm = () => {
               </CardBody>
             </Card>
           </Col>
-          <Col lg="6">
-            <Row>
-              <Col lg="12">
+          <Col lg="3">
                 <Card className="card-chart">
-                  <CardHeader>
-                    <h3 className="card-category text-center">
-                      {" "}
-                      אירועים לפי פיקוד
-                    </h3>
-                  </CardHeader>
-                  <CardBody>
-                    <h1>2</h1>
-                  </CardBody>
-                </Card>
-              </Col>
-              <Col lg="12">
-                <Card className="card-chart">
-                  <CardHeader>
+                <CardHeader>
                     <h3 className="card-category text-center">
                       {" "}
                       אירועים לפי סוג אירוע
                     </h3>
                   </CardHeader>
                   <CardBody>
-                    <h1>3</h1>
+                  <Doughnut data={dataevent} options={options}/>
+                  </CardBody>
+                </Card>
+          </Col>
+
+          <Col lg="3">
+                <Card className="card-chart">
+                  <CardHeader>
+                    <h3 className="card-category text-center">
+                      {" "}
+                      מספר אירועים לפי פיקוד
+                    </h3>
+                  </CardHeader>
+                  <CardBody>
+                  <Doughnut data={datapikod} options={options}/>
+                  </CardBody>
+                </Card>
+              </Col>
+          </Row>
+
+              <Row>
+              <Col lg="4">
+                <Card className="card-chart">
+                  <CardHeader>
+                    <h3 className="card-category text-center">
+                      {" "}
+                      מספר אירועים לפי אוגדה
+                    </h3>
+                    {(!(data.ogda)) ?
+                      <Col style={{ justifyContent: 'right', alignContent: 'right', textAlign: 'right' }}>
+                        <h6>בחר פיקוד</h6>
+                        <Select data={pikods}
+                         handleChange2={handleChange2}
+                          name={'pikod'} val={data.pikod ? data.pikod : undefined} />
+                      </Col> :
+                      <Col style={{ justifyContent: 'right', alignContent: 'right', textAlign: 'right' }}>
+                        <h6>בחר פיקוד</h6>
+                        <Select data={pikods} 
+                        handleChange2={handleChange2} 
+                        name={'pikod'} val={data.pikod ? data.pikod : undefined} isDisabled={true} />
+                      </Col>}
+                      <div style={{paddingTop: "5px"}} className="text-center">
+                  </div>
+
+                  </CardHeader>
+                  <CardBody>
+                  <Doughnut data={dataogda} options={options}/>
+                  </CardBody>
+                </Card>
+              </Col>
+
+              <Col lg="4">
+                <Card className="card-chart">
+                  <CardHeader>
+                    <h3 className="card-category text-center">
+                      {" "}
+                      מספר אירועים לפי חטיבה
+                    </h3>
+                    {((data.pikod) && !(data.hativa)) ?
+                      <Col style={{ justifyContent: 'right', alignContent: 'right', textAlign: 'right' }}>
+                        <h6>בחר אוגדה</h6>
+                        <Select data={ogdas} 
+                        handleChange2={handleChange2} 
+                        name={'ogda'} val={data.ogda ? data.ogda : undefined} />
+                      </Col> :
+                      <Col style={{ justifyContent: 'right', alignContent: 'right', textAlign: 'right' }}>
+                        <h6>בחר אוגדה</h6>
+                        <Select data={ogdas}
+                         handleChange2={handleChange2}
+                          name={'ogda'} val={data.ogda ? data.ogda : undefined} isDisabled={true} />
+                      </Col>}
+                      <div style={{paddingTop: "5px"}} className="text-center">
+                  </div>
+
+                  </CardHeader>
+                  <CardBody>
+                  <Doughnut data={datahativa} options={options}/>
+                  </CardBody>
+                </Card>
+              </Col>
+              <Col lg="4">
+                <Card className="card-chart">
+                  <CardHeader>
+                    <h3 className="card-category text-center">
+                      {" "}
+                      מספר אירועים לפי גדוד
+                    </h3>
+                    {((data.ogda) && !(data.gdod)) ?
+                      <Col style={{ justifyContent: 'right', alignContent: 'right', textAlign: 'right' }}>
+                        <h6>בחר חטיבה</h6>
+                        <Select data={hativas}
+                         handleChange2={handleChange2} 
+                         name={'hativa'} val={data.hativa ? data.hativa : undefined} />
+                      </Col> :
+                      <Col style={{ justifyContent: 'right', alignContent: 'right', textAlign: 'right' }}>
+                        <h6>בחר חטיבה</h6>
+                        <Select data={hativas}
+                         handleChange2={handleChange2}
+                          name={'hativa'} val={data.hativa ? data.hativa : undefined} isDisabled={true} />
+                      </Col>}
+                      <div style={{paddingTop: "5px"}} className="text-center">
+                  </div>
+
+                  </CardHeader>
+                  <CardBody>
+                  <Doughnut data={datagdod} options={options} />
                   </CardBody>
                 </Card>
               </Col>
             </Row>
-          </Col>
-        </Row>
+
       </Container>
     </div>
   );
